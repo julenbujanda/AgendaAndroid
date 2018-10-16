1398f01bb84c526d053c94b792b6db1049de3924
julenb <21701719@live.uem.es>
2018-10-16 17:50:23 +0200

    Realizar Llamada

--- app/src/main/java/ga/julen/agenda/ContactsActivity.java
+++ app/src/main/java/ga/julen/agenda/ContactsActivity.java
@@ -1,17 +1,24 @@
 package ga.julen.agenda;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.net.Uri;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
@@ -33,6 +40,7 @@ public class ContactsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contacts);
         listView = findViewById(R.id.list_view);
         contactos = new ArrayList<>();
+        comprobarPermisoLlamada();
         ArrayList<String> nombres = new ArrayList<>();
         try {
             nombres = leerDB();
@@ -72,7 +80,7 @@ public class ContactsActivity extends AppCompatActivity {
         return nombres;
     }
 
-    public void mostrarPopUp(View view, Contacto contacto) {
+    public void mostrarPopUp(View view, final Contacto contacto) {
         LayoutInflater inflater = (LayoutInflater)
                 getSystemService(LAYOUT_INFLATER_SERVICE);
         View popupView = inflater.inflate(R.layout.popup_window, null);
@@ -82,14 +90,24 @@ public class ContactsActivity extends AppCompatActivity {
                 LinearLayout.LayoutParams.WRAP_CONTENT, true);
 
         TextView txtPopUpNombre = popupView.findViewById(R.id.popup_nombre);
-        TextView txtPopUpTelefono = popupView.findViewById(R.id.popup_telefono);
+        Button btnPopUpTelefono = popupView.findViewById(R.id.popup_telefono);
         TextView txtPopUpMail = popupView.findViewById(R.id.popup_mail);
 
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
 
         txtPopUpNombre.setText(contacto.getNombre() + " " + contacto.getApellido());
-        txtPopUpTelefono.setText(contacto.getNumero());
+        btnPopUpTelefono.setText(contacto.getNumero());
         txtPopUpMail.setText(contacto.getMail());
+
+        btnPopUpTelefono.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + contacto.getNumero()));
+                startActivity(callIntent);
+            }
+        });
 
         popupView.setOnTouchListener(new View.OnTouchListener() {
             @Override
@@ -98,6 +116,12 @@ public class ContactsActivity extends AppCompatActivity {
                 return true;
             }
         });
+    }
+
+    private void comprobarPermisoLlamada() {
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(ContactsActivity.this, new String[]{Manifest.permission.CALL_PHONE}, 1);
+        }
     }
 
 }
