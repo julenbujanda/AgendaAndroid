406273dc41e4457e61978f2178c5a7ac7867e0f7
julenb <21701719@live.uem.es>
2018-10-16 17:04:02 +0200

    PopUp Contactos

--- app/src/main/java/ga/julen/agenda/ContactsActivity.java
+++ app/src/main/java/ga/julen/agenda/ContactsActivity.java
@@ -6,8 +6,16 @@ import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -17,20 +25,29 @@ public class ContactsActivity extends AppCompatActivity {
 
     SQLiteOpenHelper sqLite;
 
+    ArrayList<Contacto> contactos;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
         listView = findViewById(R.id.list_view);
-        ArrayList<String> nombres=new ArrayList<>();
+        contactos = new ArrayList<>();
+        ArrayList<String> nombres = new ArrayList<>();
         try {
-            nombres=leerDB();
+            nombres = leerDB();
         } catch (Exception e) {
             e.printStackTrace();
             nombres.add("No hay ningún contacto.");
         }
-        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_expandable_list_item_1, nombres);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_expandable_list_item_1, nombres);
         listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicion, long l) {
+                mostrarPopUp(view, contactos.get(posicion));
+            }
+        });
     }
 
     private ArrayList<String> leerDB() {
@@ -49,8 +66,38 @@ public class ContactsActivity extends AppCompatActivity {
         Cursor cursor = sqLite.getWritableDatabase().rawQuery("SELECT * FROM agenda", null);
         while (cursor.moveToNext()) {
             nombres.add(cursor.getString(1) + " " + cursor.getString(2));
+            contactos.add(new Contacto(cursor.getInt(0), cursor.getString(1),
+                    cursor.getString(2), cursor.getString(3), cursor.getString(4)));
         }
         return nombres;
+    }
+
+    public void mostrarPopUp(View view, Contacto contacto) {
+        LayoutInflater inflater = (LayoutInflater)
+                getSystemService(LAYOUT_INFLATER_SERVICE);
+        View popupView = inflater.inflate(R.layout.popup_window, null);
+
+
+        final PopupWindow popupWindow = new PopupWindow(popupView, LinearLayout.LayoutParams.WRAP_CONTENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT, true);
+
+        TextView txtPopUpNombre = popupView.findViewById(R.id.popup_nombre);
+        TextView txtPopUpTelefono = popupView.findViewById(R.id.popup_telefono);
+        TextView txtPopUpMail = popupView.findViewById(R.id.popup_mail);
+
+        popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+        txtPopUpNombre.setText(contacto.getNombre() + " " + contacto.getApellido());
+        txtPopUpTelefono.setText(contacto.getNumero());
+        txtPopUpMail.setText(contacto.getMail());
+
+        popupView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                popupWindow.dismiss();
+                return true;
+            }
+        });
     }
 
 }
