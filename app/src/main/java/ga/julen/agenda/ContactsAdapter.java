59d15e3751db39f08cd77d76c4f5dcfbd8b78267
julenb <21701719@live.uem.es>
2018-10-17 20:27:25 +0200

    Edición clase con implementación de Filterable

--- app/src/main/java/ga/julen/agenda/ContactsAdapter.java
+++ app/src/main/java/ga/julen/agenda/ContactsAdapter.java
@@ -5,49 +5,34 @@ import android.database.DataSetObserver;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListAdapter;
+import android.widget.BaseAdapter;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class ContactsAdapter implements ListAdapter {
+public class ContactsAdapter extends BaseAdapter implements Filterable {
 
     private ArrayList<Contacto> contactos;
+    private ArrayList<Contacto> contactosMostrados;
     private Context contexto;
 
     public ContactsAdapter(ArrayList<Contacto> contactos, Context contexto) {
-        this.contactos = contactos;
+        super();
+        //this.contactos = contactos;
+        this.contactosMostrados = contactos;
         this.contexto = contexto;
     }
 
     @Override
-    public boolean areAllItemsEnabled() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled(int i) {
-        return true;
-    }
-
-    @Override
-    public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
-    public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-
-    }
-
-    @Override
     public int getCount() {
-        return contactos.size();
+        return contactosMostrados.size();
     }
 
     @Override
     public Contacto getItem(int posicion) {
-        return contactos.get(posicion);
+        return contactosMostrados.get(posicion);
     }
 
     @Override
@@ -56,33 +41,64 @@ public class ContactsAdapter implements ListAdapter {
     }
 
     @Override
-    public boolean hasStableIds() {
-        return false;
-    }
-
-    @Override
     public View getView(int posicion, View view1, ViewGroup viewGroup) {
         LayoutInflater inflater = LayoutInflater.from(contexto);
         View view = inflater.inflate(R.layout.contacto, viewGroup, false);
         TextView txtNombre = view.findViewById(R.id.lista_nombre);
         TextView txtApellido = view.findViewById(R.id.lista_apellido);
-        txtNombre.setText(contactos.get(posicion).getNombre());
-        txtApellido.setText(contactos.get(posicion).getApellido());
+        txtNombre.setText(contactosMostrados.get(posicion).getNombre());
+        txtApellido.setText(contactosMostrados.get(posicion).getApellido());
         return view;
     }
 
     @Override
     public int getItemViewType(int i) {
-        return contactos.size();
+        return contactosMostrados.size();
     }
 
     @Override
     public int getViewTypeCount() {
-        return contactos.size();
+        return contactosMostrados.size();
     }
 
     @Override
     public boolean isEmpty() {
-        return contactos.isEmpty();
+        return contactosMostrados.isEmpty();
     }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence constraint) {
+                FilterResults results = new FilterResults();
+                ArrayList<Contacto> contactosFiltrados = new ArrayList<>();
+
+                if (contactos == null)
+                    contactos = contactosMostrados;
+                if (constraint != null) {
+                    for (Contacto contacto : contactos) {
+                        if (contacto.getNombre().toLowerCase().contains(constraint.toString().toLowerCase()))
+                            contactosFiltrados.add(contacto);
+                    }
+                    results.count = contactosFiltrados.size();
+                    results.values = contactosFiltrados;
+                }
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                contactosMostrados = (ArrayList<Contacto>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+
+
+    }
+
+    public void notifyDataSetChanged() {
+        super.notifyDataSetChanged();
+    }
+
 }
